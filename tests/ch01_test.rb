a75c020fb14af23dcf2b85f16fbeaf1475574c9a
Islam Farg <islam3zzat@gmail.com>
2023-04-02 12:40:51 +0100

    remove unneeded requirements

--- tests/ch01_test.rb
+++ tests/ch01_test.rb
@@ -1,7 +1,5 @@
 require "minitest"
 require "minitest/autorun"
-require "minitest/spec"
-require "minitest/pride"
 require_relative "../lib/ch01"
 
 describe Customer do
@@ -9,8 +7,8 @@ describe Customer do
   let(:rent) do
     lambda { |movie, days| subject.add_rental(Rental.new(movie, days)) }
   end
-  let(:regular_movie)   { Movie.new "Regent", RegularPrice.new }
-  let(:new_movie)       { Movie.new "Newton", NewReleasePrice.new }
+  let(:regular_movie) { Movie.new "Regent", RegularPrice.new }
+  let(:new_movie) { Movie.new "Newton", NewReleasePrice.new }
   let(:childrens_movie) { Movie.new "Chills", ChildrensPrice.new }
 
   it "has a name" do
@@ -26,8 +24,6 @@ describe Customer do
     assert_equal :movie2, subject.rentals[1].movie
   end
 
-
-
   describe "#statement" do
     it "outputs a String" do
       assert_instance_of String, subject.statement
@@ -40,7 +36,7 @@ describe Customer do
     describe "rental costs for 1 day" do
       before do
         rent[regular_movie, 1]
-        rent[new_movie,     1]
+        rent[new_movie, 1]
         rent[childrens_movie, 1]
       end
 
@@ -87,16 +83,16 @@ describe Customer do
       it "prints the statement as expected" do
         #                          fee     points
         # =======================================
-        rent[regular_movie, 2]   # 2       1
-        rent[regular_movie, 3]   # 3.5     1
-        rent[new_movie, 1]       # 3       1
-        rent[new_movie, 3]       # 9       2
+        rent[regular_movie, 2] # 2       1
+        rent[regular_movie, 3] # 3.5     1
+        rent[new_movie, 1] # 3       1
+        rent[new_movie, 3] # 9       2
         rent[childrens_movie, 3] # 1.5     1
         rent[childrens_movie, 4] # 3       1
         # =======================================
         #              total:      22.0    7
 
-        expected = <<~STATEMENT
+        expected = <<~STATEMENT.chomp
           Rental Record for Smith
           \tRegent\t2
           \tRegent\t3.5
@@ -107,7 +103,6 @@ describe Customer do
           Amount owed is 22.0
           You earned 7 frequent renter points
         STATEMENT
-        .chomp
         assert_equal expected, subject.statement
       end
     end
@@ -119,29 +114,32 @@ describe Customer do
     end
 
     it "includes a header line with the customer’s name" do
-      assert_includes subject.html_statement, "<h1>Rental Record for <em>Smith</em>\n</h1>"
+      assert_includes subject.html_statement,
+                      "<h1>Rental Record for <em>Smith</em>\n</h1>"
     end
 
     describe "rental costs for 1 day" do
       before do
         rent[regular_movie, 1]
-        rent[new_movie,     1]
+        rent[new_movie, 1]
         rent[childrens_movie, 1]
       end
 
       it "includes movie title and fee for each rented movie" do
         s = subject.html_statement
-        assert_match /^\tRegent\t2<br \/>$/, s
-        assert_match /^\tNewton\t3<br \/>$/, s
-        assert_match /^\tChills\t1\.5<br \/>$/, s
+        assert_match %r{^\tRegent\t2<br />$}, s
+        assert_match %r{^\tNewton\t3<br />$}, s
+        assert_match %r{^\tChills\t1\.5<br />$}, s
       end
 
       it "includes the total fee for all rentals" do
-        assert_includes subject.html_statement, "<p>Amount owed is <em>6.5</em></p>\n"
+        assert_includes subject.html_statement,
+                        "<p>Amount owed is <em>6.5</em></p>\n"
       end
 
       it "includes frequent renter points earned" do
-        assert_includes subject.html_statement, "You earned <em>3</em> frequent renter points"
+        assert_includes subject.html_statement,
+                        "You earned <em>3</em> frequent renter points"
       end
     end
 
@@ -150,21 +148,24 @@ describe Customer do
         rent[new_movie, 1]
         rent[new_movie, 2] # <-- bonus point
         rent[regular_movie, 3]
-        assert_includes subject.html_statement, "You earned <em>4</em> frequent renter points"
+        assert_includes subject.html_statement,
+                        "You earned <em>4</em> frequent renter points"
       end
     end
 
     describe "regular movie, more than two days" do
       it "costs 2 dollars plus 1.5 dollars for each additional day" do
         rent[regular_movie, 4]
-        assert_match /^\tRegent\t#{2 + 1.5 * 2}<br \/>$/, subject.html_statement
+        assert_match %r{^\tRegent\t#{2 + 1.5 * 2}<br />$},
+                     subject.html_statement
       end
     end
 
     describe "childrens movie, more than three days" do
       it "costs 1.5 dollars plus 1.5 dollars for each additional day" do
         rent[childrens_movie, 6]
-        assert_match /^\tChills\t#{1.5 + 1.5 * 3}<br \/>$/, subject.html_statement
+        assert_match %r{^\tChills\t#{1.5 + 1.5 * 3}<br />$},
+                     subject.html_statement
       end
     end
 
@@ -172,16 +173,16 @@ describe Customer do
       it "prints the statement as expected" do
         #                          fee     points
         # =======================================
-        rent[regular_movie, 2]   # 2       1
-        rent[regular_movie, 3]   # 3.5     1
-        rent[new_movie, 1]       # 3       1
-        rent[new_movie, 3]       # 9       2
+        rent[regular_movie, 2] # 2       1
+        rent[regular_movie, 3] # 3.5     1
+        rent[new_movie, 1] # 3       1
+        rent[new_movie, 3] # 9       2
         rent[childrens_movie, 3] # 1.5     1
         rent[childrens_movie, 4] # 3       1
         # =======================================
         #              total:      22.0    7
 
-        expected = <<~STATEMENT
+        expected = <<~STATEMENT.chomp
           <h1>Rental Record for <em>Smith</em>\n</h1>
           \tRegent\t2<br />
           \tRegent\t3.5<br />
@@ -192,7 +193,6 @@ describe Customer do
           <p>Amount owed is <em>22.0</em></p>
           You earned <em>7</em> frequent renter points
         STATEMENT
-        .chomp
         assert_equal expected, subject.html_statement
       end
     end
@@ -210,4 +210,3 @@ describe Rental do
     assert_equal 5, subject.days_rented
   end
 end
-
