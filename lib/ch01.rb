409d394e960def849583e90e3fae1a81ecb02f23
Islam Farg <islam3zzat@gmail.com>
2023-04-02 10:37:20 +0100

    starting point

--- lib/ch01.rb
+++ lib/ch01.rb
@@ -1,122 +1,75 @@
-require 'pry'
+require "pry"
+
+class Movie
+  REGULAR = 0
+  NEW_RELEASE = 1
+  CHILDRENS = 2
+
+  attr_reader :title, :price_code
+
+  def initialize(title, price_code)
+    @title = title
+    @price_code = price_code
+  end
+end
+
+class Rental
+  attr_reader :movie, :days_rented
+
+  def initialize(movie, days_rented)
+    @movie = movie
+    @days_rented = days_rented
+  end
+end
 
 class Customer
-  attr_reader :name, :rentals
+  attr_reader :name
 
   def initialize(name)
     @name = name
     @rentals = []
   end
 
-  def html_statement
-    result = "<h1>Rental Record for <em>#{name}</em>\n</h1>\n"
-
-    rentals.each do |rental|
-      # show figures for this rental
-      result += "\t" + rental.movie.title + "\t" + rental.charge.to_s + "<br />\n"
-    end
-
-    # add footer lines
-    result += "<p>Amount owed is <em>#{total_charge}</em></p>\n"
-    result += "You earned <em>#{total_frequent_renter_points}</em> frequent renter points"
-    result
+  def add_rental(rental)
+    @rentals << rental
   end
 
   def statement
-    result = "Rental Record for #{name}\n"
+    total_amount = 0
+    frequent_renter_points = 0
+    result = "Rental Record for #{@name}\n"
 
-    rentals.each do |rental|
+    @rentals.each do |rental|
+      this_amount = 0
+
+      # determine amount for each rental
+      case rental.movie.price_code
+      when Movie::REGULAR
+        this_amount += 2
+        this_amount += (rental.days_rented - 2) * 1.5 if rental.days_rented > 2
+      when Movie::NEW_RELEASE
+        this_amount += rental.days_rented * 3
+      when Movie::CHILDRENS
+        this_amount += 1.5
+        this_amount += (rental.days_rented - 3) * 1.5 if rental.days_rented > 3
+      end
+
+      # add frequent renter points
+      frequent_renter_points += 1
+      # add bonus for a two-day new release rental
+      if rental.movie.price_code == Movie::NEW_RELEASE && rental.days_rented > 1
+        frequent_renter_points += 1
+      end
+
       # show figures for this rental
-      result += "\t" + rental.movie.title + "\t" + rental.charge.to_s + "\n"
+      result += "\t#{rental.movie.title}\t#{this_amount}\n"
+      total_amount += this_amount
     end
 
     # add footer lines
-    result += "Amount owed is #{total_charge}\n"
-    result += "You earned #{total_frequent_renter_points} frequent renter points"
+    result += "Amount owed is #{total_amount}\n"
+    result += "You earned #{frequent_renter_points} frequent renter points"
+
     result
-  end
-
-  def add_rental(rental)
-    rentals << rental
-  end
-
-  private
-
-  def total_charge
-    rentals.inject(0) { |sum, rental| sum + rental.charge }
-  end
-
-  def total_frequent_renter_points
-    rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
-  end
-
-end
-class Movie
-  REGULAR = 0
-  NEW_RELEASE = 1
-  CHILDRENS = 2
-
-  attr_reader :title, :price
-
-  def initialize(title, price)
-    @title, @price = title, price
-  end
-
-  def charge(days_rented)
-    price.charge(days_rented)
-  end
-
-  def frequent_renter_points(days_rented)
-    price.frequent_renter_points(days_rented)
-  end
-
-end
-class RegularPrice
-  def charge(days_rented)
-    result = 2
-    result += (days_rented - 2) * 1.5 if days_rented > 2
-    result
-  end
-
-  def frequent_renter_points(days_rented)
-    1
-  end
-end
-class NewReleasePrice
-  def charge(days_rented)
-    days_rented * 3
-  end
-
-  def frequent_renter_points(days_rented)
-    days_rented > 1 ? 2 : 1
-  end
-end
-class ChildrensPrice
-  def charge(days_rented)
-    result = 1.5
-    result += (days_rented - 3) * 1.5 if days_rented > 3
-    result
-  end
-
-  def frequent_renter_points(days_rented)
-    1
-  end
-end
-
-
-class Rental
-  attr_reader :movie, :days_rented
-
-  def initialize(movie, days_rented)
-    @movie, @days_rented = movie, days_rented
-  end
-
-
-  def frequent_renter_points
-    movie.frequent_renter_points(days_rented)
-  end
-
-  def charge
-    movie.charge(days_rented)
   end
 end
